319357456287278c81afff286ce2727ee171a321
Gina Roma <gina.roma1@marist.edu>
2020-02-03 19:46:27 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,6 @@
+noun=input("enter a noun")
+verb=input("enter a verb")
+adjective=input("enter a adjective")
+place=input("enter a place")
+print("This",noun,"is",verb,"very",adjective,"in the",place,".")
+
