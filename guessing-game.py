06b972d0ccfd27dbe1d5dda29fe4ed64ae2533cc
Gina Roma <gina.roma1@marist.edu>
2020-02-17 18:35:03 -0500

    Usability improvements to the guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -4,17 +4,22 @@ def main():
     print("PYTHON GUESSING GAME. ")
     
     print()
-    animal = ("Dog")
+    animal = ("dog")
     x=0
+    q = "quit"
     while x==0:
         print("This program is thinking of an animal; he/she is mans best friend\nWhat is the animal this program is thinking? ")
         print()
         answer = input("Enter choice here\n")
+        answer = answer.lower()
         if answer == animal:
-            print("Congratuations you have won!")
+            print("Congratulations you have won!")
+            x=1
+            
+        elif answer[0] == "q":
             x=1
         else:
-            print("Sorry your answer is inncorrect would you like to try again? ")
-
-        
+            print("Sorry your answer is inncorrect would you like to try again?")
+            print()
+    
 main()
