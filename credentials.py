ca108f6b954a74b614760aadf7be5a804bd39667
Gina Roma <gina.roma1@marist.edu>
2020-03-05 16:21:06 -0500

    Working with Strings and Functions 3rd commit

--- credentials.py
+++ credentials.py
@@ -3,6 +3,7 @@
 # Author: Gina Roma 
 # Created: 2020-02-24
 
+import sys
 
 def name():
     # get user's first and last names
@@ -13,31 +14,41 @@ def name():
 
 
 def password():
-    passwd = str(input("Create a new password: "))
-    
-    while len(passwd) < 8:
-        print("Fool of a Took! That password is feeble!\nThis password is weak")
-        passwd = input("Create a new password with 8 characters: ")
-        print(passwd, "\nThis password is strong")
-    
+    passwd = input("Create a new password: ")
+    if checkStrength(passwd):
+        print("Fool of a Took! That password is feeble!")
+        print("This password is weak. You need at least one uppercase letter, one lowercase letter,\nand your password has to be 8 characters long.")
+        return password() 
+    else:
+       print("The force is strong in this one…") 
     return passwd
+
+# This checks the strength of the password
+def checkStrength(passwd):
+    return len(passwd) < 8 or passwd.islower() or passwd.isupper() or passwd.isnumeric()
     
-    
-    
+                   
 def username():
     first, last = name()
     uname = str(first + "." + last)
     return uname
-    
-               
+                 
 def main():
-    uname = username()
+    uname = username().lower()
 
     passwd = password()
     
     print("Account configured. Your new email address is",uname, "@marist.edu")
     print("Your username is", uname, ".")
     print("Your password is", passwd, ".")
-
+    sys.exit
     
 main()
+
+
+
+
+
+
+
+
